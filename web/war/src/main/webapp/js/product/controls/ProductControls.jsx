84c92e376c69e080409908c2c8ea0d09250159a0
Joey Burke <joeybrk372@gmail.com>
2017-09-12 15:13:00 -0400

    Remove unnecessary component

--- web/war/src/main/webapp/js/product/controls/ProductControls.jsx
+++ web/war/src/main/webapp/js/product/controls/ProductControls.jsx
@@ -1,19 +1,21 @@
 define([
     'create-react-class',
     'prop-types',
-    'components/Attacher',
-    './NavigationControls',
-    './ProductControlsList'
+    './ProductControlsOption',
+    './ProductControlsMenu'
 ], function(
     createReactClass,
     PropTypes,
-    Attacher,
-    NavigationControls,
-    ProductControlsList) {
+    ProductControlsOption,
+    ProductControlsMenu) {
     'use strict';
-    const navigationButtons = [
-        { identifier: 'product-navigation-zoom-in', }
-    ];
+
+    const placementHint = {
+        MENU: 'menu',
+        BUTTON: 'button',
+        DROPDOWN: 'dropdown'
+    };
+    const MENU_IDENTIFIER = 'menu';
 
     const ProductControls = createReactClass({
 
@@ -21,17 +23,15 @@ define([
             tools: PropTypes.arrayOf(PropTypes.shape({
                 identifier: PropTypes.string.isRequired,
                 componentPath: PropTypes.string.isRequired,
-                props: PropTypes.object,
-                placementHint: PropTypes.string,
-                icon: PropTypes.string,
-                label: PropTypes.string
+                button: PropTypes.shape({
+                    icon: PropTypes.string,
+                    label: PropTypes.string
+                }),
+                props: PropTypes.object
             })),
-            rightOffset: PropTypes.number,
-            zoom: PropTypes.bool,
-            pan: PropTypes.bool,
+            onFit: PropTypes.func,
             onZoom: PropTypes.func,
-            onPan: PropTypes.func,
-            onFit: PropTypes.func
+            rightOffset: PropTypes.number
         },
 
         getDefaultProps() {
@@ -41,16 +41,115 @@ define([
             }
         },
 
+        getInitialState() {
+            return {
+                activeOption: null
+            }
+        },
+
+        componentDidMount() {
+            $(document).on('keydown.org-visallo-graph-product-controls', (event) => {
+                if (event.which === 27) { //esc
+                    this.setState({ activeOption: null });
+                }
+            });
+        },
+
+        componentWillUnmount() {
+            $(document).off('keydown.org-visallo-graph-product-controls');
+        },
+
         render() {
-            const { tools, rightOffset, onFit, ...navigationProps } = this.props;
-            const { onPan, onZoom } = navigationProps;
+            const { activeOption } = this.state;
+            const { onFit, onZoom, tools, rightOffset } = this.props;
+            const menuOptions = [], listOptions = [];
+            const groupByPlacement = (tool) => {
+               const { placementHint, icon, label, componentPath, handler } = tool;
+
+               if (placementHint) {
+                   if (placementHint === placementHint.MENU) {
+                       menuOptions.push(tool);
+                   } else {
+                       listOptions.push(tool);
+                   }
+               } else if (icon || label || handler) {
+                   listOptions.push(tool);
+               } else {
+                   menuOptions.push(tool);
+               }
+            };
+
+            tools.concat(this.getDefaultOptions()).forEach(groupByPlacement);
 
             return (
                 <div className="product-controls" style={{transform: `translate(-${rightOffset}px, 0)`}}>
-                    <ProductControlsList tools={tools} onFit={onFit} rightOffset={rightOffset} onZoom={onZoom} />
+                    <div className="controls-list">
+                        {listOptions.length ?
+                            <ul className="extensions">
+                                {listOptions.map(tool => (
+                                    <ProductControlsOption
+                                        tool={tool}
+                                        active={activeOption === tool.identifier}
+                                        key={tool.identifier}
+                                        onClick={() => {this.onItemClick(tool.identifier) }}
+                                        rightOffset={rightOffset}
+                                    />
+                                ))}
+                            </ul>
+                        : null}
+                        {menuOptions.length ?
+                            <ProductControlsMenu
+                                options={menuOptions}
+                                active={activeOption === MENU_IDENTIFIER}
+                                onToggle={() => { this.onItemClick(MENU_IDENTIFIER) }}
+                            />
+                        : null}
+                    </div>
                 </div>
             );
         },
+
+        onItemClick(identifier) {
+            if (identifier) {
+                const activeOption = this.state.activeOption;
+
+                this.setState({
+                    activeOption: activeOption === identifier ? null : identifier
+                });
+            }
+        },
+
+        getDefaultOptions() {
+            const defaultOptions = [];
+            const { onZoom, onFit } = this.props;
+
+            if (onZoom) {
+                defaultOptions.push({
+                    identifier: 'org-visallo-product-zoom-in',
+                    placementHint: 'button',
+                    label: '+',
+                    props: { handler: _.partial(onZoom, 'in') },
+                    buttonClass: 'zoom'
+                }, {
+                    identifier: 'org-visallo-product-zoom-out',
+                    placementHint: 'button',
+                    label: '-',
+                    props: { handler: _.partial(onZoom, 'out') },
+                    buttonClass: 'zoom'
+                })
+            }
+
+            if (onFit) {
+                defaultOptions.push({
+                    identifier: 'org-visallo-product-fit',
+                    placementHint: 'button',
+                    label: 'Fit',
+                    props: { handler: onFit}
+                })
+            }
+
+            return defaultOptions;
+        }
     });
 
     return ProductControls;
